170a302373e2ad5fcd5690a7e29b987a6d83e240
luiz ricardo <luizricardor852@gmail.com>
2023-12-14 20:13:55 -0300

    novo repositorio

---
+++ idades.js
@@ -0,0 +1,15 @@
+let olderPersonName= prompt("digite o nome da pessoa mais velha ");
+let olderPersonAge= prompt("digite a idade da pessoa mais velha");
+ 
+let youngerPersonName = prompt("digite o nome da pessoa mais nova");
+let youngerPersonAge = prompt("digite a idade da pessoa mais nova")
+
+let ageDifference = olderPersonAge-youngerPersonAge
+
+
+  alert(
+   "pessoa mais velha:"+ olderPersonName + "\nIdade:"+ olderPersonAge +
+   "\n\npessoa mais nova:" + youngerPersonName + "\nIdade:"+ youngerPersonAge +
+   "\n\nA diferença de idade:"+ ageDifference
+
+  )
