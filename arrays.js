512a491ad61a31cb4c278b03015dee2a34401813
luiz ricardo <luizricardor852@gmail.com>
2023-12-15 05:56:52 -0300

    arrais e funçoes

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let spaceships = ["Colossus", "Ártemis", "Fênix"]
+
+let [colossus, artemis, fenix, puller] = spaceships
+//let colossus = spaceships [0]
+//et artemis = spaceships[1]
+
+console. log(colossus, artemis, fenix, puller)
---
+++ funcoes.js
@@ -0,0 +1,6 @@
+let spaceships = ["Colossus", "Artemis", "Fênix"]
+
+function printSpaceships([colossus, artemis, fenix, puller]) {
+console. log(colossus, artemis, fenix, puller)
+}
+printSpaceships (spaceships)
